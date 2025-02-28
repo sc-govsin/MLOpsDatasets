8e4564625b96a14e89b65272de9723f7a2188add
Govind Singh <126751983+sc-govsin@users.noreply.github.com>
2025-02-28 15:07:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Govind Singh",
     author_email="govind.singh@scryai.com",
     description="A Python library to download datasets created with Scry.",
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=open("README.MD", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/sc-govsin/ScryDatasets",
     classifiers=[
